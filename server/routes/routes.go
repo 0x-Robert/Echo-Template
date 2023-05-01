6aad7bac4d9877c2c098b82fd9556fe8b2c4567d
shypang <smartdragon417@gmail.com>
2023-05-01 22:50:28 +0900

    Study: echo

---
+++ server/routes/routes.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	s "echo-demo-project/server"
+)
+
+func ConfigureRoutes(server *s.Server) {
+	postHandler := handlers.NewPostHandlers(server)
+	// authHandler := handlers.NewAuthHandler(server)
+	// registerHandler := handlers.NewRegisterHandler(server)
+
+	// server.Echo.Use(middleware.Logger())
+
+	// server.Echo.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	// server.Echo.POST("/login", authHandler.Login)
+	// server.Echo.POST("/register", registerHandler.Register)
+	// server.Echo.POST("/refresh", authHandler.RefreshToken)
+
+	// fmt.Println(server.Config.Auth.AccessSecret)
+
+	// r := server.Echo.Group("")
+	// config := middleware.JWTConfig{
+	// 	Claims:     &token.JwtCustomClaims{},
+	// 	SigningKey: []byte(server.Config.Auth.AccessSecret),
+	// }
+	// r.Use(middleware.JWTWithConfig(config))
+
+	// r.GET("/posts", postHandler.GetPosts)
+	// r.POST("/posts", postHandler.CreatePost)
+	// r.DELETE("/posts/:id", postHandler.DeletePost)
+	// r.PUT("/posts/:id", postHandler.UpdatePost)
+}
